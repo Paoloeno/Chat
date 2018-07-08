67822c4f1c4074be3460e2b5c9511365825d5746
Paoloeno <p.nowosielski93@gmail.com>
2018-07-08 18:12:34 +0200

    aa

--- src/main/java/pl/nowosielski/chat/models/ChatSocket.java
+++ src/main/java/pl/nowosielski/chat/models/ChatSocket.java
@@ -17,31 +17,25 @@ public class ChatSocket extends TextWebSocketHandler implements WebSocketConfigu
 
     public List <WebSocketSession> userList = new ArrayList<>();
 
-    //Dodajemy handlera do websocketa. W związku z tym, że klasa ChatSocket jest websocketHandlerem (rozszerza TextWebSocketHandler) możemy wpisać po prostu słówko kluczowe "this". Drugi parametr to adres pod jakim się można połączyć z socketem. Metoda setAllowedOrigins określa z jakich adresów ip możemy się podłączyć do czatu - tutaj gwiazdka oznacza wszystkie.
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
         webSocketHandlerRegistry.addHandler(this, "/room").setAllowedOrigins("*");
     }
 
-    //Co się ma stać jak ktoś nawiąże połączenie z naszym socketem
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         userList.add(session);
         System.out.println("Podłączył się 1 użytkownik");
     }
 
-    //Co się ma stać jak ktoś wyjdzie
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
         userList.remove(session);
         System.out.println("Rozłączył się 1 użytkownik");
     }
 
-    //Co ma się stać, gdy ktoś wyśle wiadomość
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-
-        //Gdy ktoś wyśle wiadomość, to iterujemy po wszystkich użytkownikach i do wszystkich ją przesyłamy
         userList.forEach(s -> {
             try {
                 s.sendMessage(new TextMessage(message.getPayload()));
