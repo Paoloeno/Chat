30aa3f86a05a61013cd83dd5c245138ed4e4815e
Paoloeno <p.nowosielski93@gmail.com>
2018-07-08 18:02:31 +0200

    moved js file out from html

---
+++ src/main/resources/static/script.js
@@ -0,0 +1,21 @@
+var webSocketLink = "ws://localhost:8080/room";
+var infoText = document.getElementById("info");
+var messageArea = document.getElementById("messageArea");
+var sendedMessages = document.getElementById("messages");
+
+var webSocket = new WebSocket(webSocketLink);
+
+webSocket.onopen = function (ev) {
+    infoText.innerHTML = "Połączono";
+}
+
+webSocket.onmessage = function (ev) {
+    sendedMessages.innerHTML = sendedMessages.innerHTML + "<li>" + ev.data + "</li>"
+    sendedMessages.scrollTop = sendedMessages.scrollHeight;
+}
+
+function sendMessage() {
+    var text = messageArea.value;
+    messageArea.value = "";
+    webSocket.send(text);
+}
