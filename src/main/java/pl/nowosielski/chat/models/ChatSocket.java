ca63498f8d301541ff717f8c539344737ca0e7c3
Paoloeno <p.nowosielski93@gmail.com>
2018-07-08 18:43:58 +0200

    showing last 10 messages for new users

--- src/main/java/pl/nowosielski/chat/models/ChatSocket.java
+++ src/main/java/pl/nowosielski/chat/models/ChatSocket.java
@@ -10,12 +10,14 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 @Component
 public class ChatSocket extends TextWebSocketHandler implements WebSocketConfigurer{
 
-    public List <WebSocketSession> userList = new ArrayList<>();
+    public List <WebSocketSession> userList = new LinkedList<>();
+    public List <String> lastTenMessages = new LinkedList<>();
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
@@ -26,6 +28,14 @@ public class ChatSocket extends TextWebSocketHandler implements WebSocketConfigu
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         userList.add(session);
         System.out.println("Podłączył się 1 użytkownik");
+
+        lastTenMessages.forEach(s -> {
+            try {
+                session.sendMessage(new TextMessage(s));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
     }
 
     @Override
@@ -36,6 +46,7 @@ public class ChatSocket extends TextWebSocketHandler implements WebSocketConfigu
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        addMessageToLastTenList(message.getPayload());
         userList.forEach(s -> {
             try {
                 s.sendMessage(new TextMessage(message.getPayload()));
@@ -43,5 +54,11 @@ public class ChatSocket extends TextWebSocketHandler implements WebSocketConfigu
                 e.printStackTrace();
             }
         });
+    }
+
+    public void addMessageToLastTenList(String message){
+        if(lastTenMessages.size() >= 10)
+            lastTenMessages.remove(0);
+        lastTenMessages.add(message);
     }
 }
